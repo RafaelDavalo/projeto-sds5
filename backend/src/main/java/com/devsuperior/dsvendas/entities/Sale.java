de435fbce7c12816f97f937a719ac439eadc9f94
RafaelDavalo <rafaeldavalo@hotmail.com>
2021-11-03 12:17:52 -0400

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -11,9 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_sales")
-public class Sale {
-	
-	
+public class Sale {	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,10 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
   <div className="container">
     <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/rafael-davalo-5aa788192/" target="_blank" rel="noreferrer">Rafael Davalo</a></p>
+    <p className="text-light"><small>Repositório no <a href="https://github.com/RafaelDavalo/projeto-sds5" target="_blank" rel="noreferrer">GitHub</a></small></p>
     <p className="text-light"><small><strong>Semana Spring React</strong><br/>
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      
   </div>
 </footer>
     );
