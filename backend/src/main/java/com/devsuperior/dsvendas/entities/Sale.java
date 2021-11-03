4c483c14407fdeac3c49b7060ddc9bd2a4655451
RafaelDavalo <rafaeldavalo@hotmail.com>
2021-11-03 10:13:40 -0400

    commit teste

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class Sale {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private Integer visited;
 	private Integer deals;
