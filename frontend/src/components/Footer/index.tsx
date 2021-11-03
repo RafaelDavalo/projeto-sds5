e350b5a746548dccf68226fbab952fcca54dc0a4
RafaelDavalo <rafaeldavalo@hotmail.com>
2021-11-03 13:11:55 -0400

    Pagination

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
   <div className="container">
-    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/rafael-davalo-5aa788192/" target="_blank" rel="noreferrer">Rafael Davalo</a></p>
-    <p className="text-light"><small>Repositório no <a href="https://github.com/RafaelDavalo/projeto-sds5" target="_blank" rel="noreferrer">GitHub</a></small></p>
+    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/rafael-davalo-5aa788192/" target="_blank" rel="noreferrer">Rafael Davalo</a><br/>
+    <small>Repositório no <a href="https://github.com/RafaelDavalo/projeto-sds5" target="_blank" rel="noreferrer">GitHub</a></small></p>
     <p className="text-light"><small><strong>Semana Spring React</strong><br/>
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       
